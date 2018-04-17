de1c706fc32d7ac213e9a0c36357048ef3a5878e
Sergey Bashtoviy <sergey.bashtoviy@outlook.com>
2018-04-17 17:00:55 +0300

    Fix answers list sort

--- src/Components/AnswersList/Container.js
+++ src/Components/AnswersList/Container.js
@@ -1,6 +1,16 @@
-import { compose, withStateHandlers, withHandlers, lifecycle, branch, renderComponent } from 'recompose';
+import {
+  compose,
+  withStateHandlers,
+  withHandlers,
+  lifecycle,
+  branch,
+  renderComponent,
+  withProps
+} from 'recompose';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
+import * as R from 'ramda';
+
 import { db } from '../../utils';
 
 import AppLoader from '../Loaders/AppLoader';
@@ -8,12 +18,43 @@ import Component from './Component';
 
 const mapStateToProps = state => ({
   user: state.user,
-  sortBy: state.answerSort
+  sortBy: state.answerSort,
 });
+
+const voteRateProp = R.prop('voteRate');
+const createdAtProp = R.prop('createdAt');
+const answerIdProp = R.prop('answerId');
+
+const SORT_TYPES = {
+  time: R.descend(createdAtProp),
+  best: R.descend(voteRateProp),
+  worst: R.ascend(voteRateProp),
+}
+
+const sortAnswers = sortType => R.sort(SORT_TYPES[sortType]);
+
+const answerRate = R.reduce(((totalRate, vote) =>
+  (vote.isPositive ? totalRate + 1 : totalRate - 1)), 0);
+
+const answersWithRate = R.curry((voteRates, answers) => R.map(
+  answer => R.merge(answer, voteRates[answer._id]),
+  answers,
+));
+
+const rateAnswers = (answers, votes) => R.pipe(
+  R.groupBy(answerIdProp),
+  R.mapObjIndexed(answerVotes => ({ voteRate: answerRate(answerVotes) })),
+  answersWithRate(R.__, answers),
+)(votes);
+
+const prepareAnswers = ({ answers, votes, sortBy }) =>
+  R.pipe(rateAnswers, sortAnswers(sortBy))(answers,votes);
 
 const enhance = compose(
   connect(mapStateToProps),
-  withStateHandlers({ answers: [], users: [], votes: [], isFetching: true }),
+  withStateHandlers({
+    answers: [], users: [], votes: [], isFetching: true,
+  }),
 
   withRouter,
 
@@ -31,17 +72,19 @@ const enhance = compose(
 
         const users = await db.users.find();
 
-        this.setState({ answers, votes, users, isFetching: false });
+        this.setState({
+          answers, votes, users, isFetching: false,
+        });
       });
     },
     componentWillUnmount() {
       clearInterval(this.interval);
-    }
+    },
   }),
 
   branch(
     ({ isFetching }) => isFetching,
-    renderComponent(AppLoader)
+    renderComponent(AppLoader),
   ),
 
   withHandlers({
@@ -54,8 +97,10 @@ const enhance = compose(
           createdById: user._id,
         });
       }
-    }
+    },
   }),
+
+  withProps(props => ({ answers: prepareAnswers(props) }))
 );
 
 
